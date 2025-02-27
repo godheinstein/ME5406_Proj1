93ead1b8bde7bc3381be9fffe1a14363aa664220
godheinstein <zhaczshth@gmail.com>
2025-02-27 08:44:31 +0800

    update 27/2

--- frozenlake_env.py
+++ frozenlake_env.py
@@ -2,19 +2,25 @@ import os
 import random
 import numpy as np
 import pygame
-from collections import defaultdict 
-from collections import deque
+from collections import defaultdict, deque
+
 
 class FrozenLakeEnv:
     def __init__(self, grid_size=4, hole_fraction=0.25, use_default_map = False):
         self.grid_size = grid_size
         self.hole_fraction = hole_fraction
         self.use_default_map = use_default_map  
+        self.state = (0,0)
         self.actions = {0: 'UP', 1: 'DOWN', 2: 'RIGHT', 3: 'LEFT'}
         self.map = self.generate_map()
-        self.state = self.find_start_position()
-        self.goal = self.find_goal_position()
+        self.goal = (grid_size -1, grid_size -1)
         self.holes = self.find_holes()
+        self.path_dir = {}
+        self.path_store_dir = {}
+        self.i = 0 
+        self.store_path = True
+        self.longest = 0
+        self.shortest = 0
         self.window_size = 400
         self.cell_size = self.window_size // grid_size
         pygame.init()
@@ -22,7 +28,6 @@ class FrozenLakeEnv:
         pygame.display.set_caption('Frozen Lake')
 
         images_folder = os.path.join(os.path.dirname(__file__), 'images')
-        self.images = images_folder 
         self.robot_img = pygame.image.load(os.path.join(images_folder, 'robot.png'))
         self.goal_img = pygame.image.load(os.path.join(images_folder, 'frisbee.png'))
         self.hole_img = pygame.image.load(os.path.join(images_folder, 'hole.png'))
@@ -32,11 +37,6 @@ class FrozenLakeEnv:
         self.hole_img = pygame.transform.scale(self.hole_img, (self.cell_size, self.cell_size))
         self.goal_img = pygame.transform.scale(self.goal_img, (self.cell_size, self.cell_size))
         self.bg_img = pygame.transform.scale(self.bg_img, (self.window_size, self.window_size))
-
-        self.map = self.generate_map()
-        self.state = self.find_start_position()
-        self.goal = self.find_goal_position()
-        self.holes = self.find_holes()
         
     def generate_map(self):
         if self.use_default_map:
@@ -61,9 +61,9 @@ class FrozenLakeEnv:
             else: 
                 raise ValueError('Default map only available for 4x4 and 10x10 grids.')
         else: 
-            return self.generate_random_map()
+            return self.generate_random_map_with_path()
         
-    def generate_random_map(self):
+    def generate_random_map_with_path(self):
         while True:
             map = np.zeros((self.grid_size, self.grid_size), dtype=int)
             map[0, 0] = 2 
@@ -77,7 +77,7 @@ class FrozenLakeEnv:
                     map[r, c] = 1
                     holes_added += 1
 
-            # Check if there's a valid path from start to goal
+            # check if path valid
             if self.is_path_available(map):
                 return map
     
@@ -95,38 +95,76 @@ class FrozenLakeEnv:
                 continue
             visited.add(current)
 
-            for action in self.actions.values():
-                nr, nc = current[0] + action[0], current[1] + action[1]
+            moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            for dr, dc in moves:
+                nr, nc = current[0] + dr, current[1] + dc
                 if 0 <= nr < self.grid_size and 0 <= nc < self.grid_size:
                     if map[nr, nc] != 1:  
                         queue.append((nr, nc))
-
         return False        
 
-    def find_start_position(self): 
-        return tuple(np.argwhere(self.map == 2)[0])
-    
-    def find_goal_position(self):
-        return tuple(np.argwhere(self.map == 3)[0])
-
+    # extract the positions of holes from the map
     def find_holes(self):
-        return set(tuple(hole) for hole in np.argwhere(self.map == 1))
-
+        
+        holes = set()
+        for i in range(self.grid_size):
+            for j in range(self.grid_size):
+                if self.map[i, j] == 1:
+                    holes.add((i, j))
+        return holes
+    # convert (row, col) tuple to an integer index
+    def position_transition(self, row, col):
+        
+        return row * self.grid_size + col
+    # reset the environment and return the initial state
     def reset(self):
-        self.state = self.find_start_position()
-        return self.state
+        
+        self.state = (0, 0)
+        self.path_dir = {}
+        self.i = 0
+        return self.position_transition(*self.state)
 
     def step(self, action):
-        move = self.actions.get(action, (0, 0))
-        new_state = (max(0, min(self.grid_size - 1, self.state[0] + move[0])),
-                     max(0, min(self.grid_size - 1, self.state[1] + move[1])))
+        move = {
+            0: (-1, 0),  # UP
+            1: (1, 0),   # DOWN
+            2: (0, 1),   # RIGHT
+            3: (0, -1)   # LEFT
+        }.get(action, (0, 0))
+
+        new_state = (
+            max(0, min(self.grid_size - 1, self.state[0] + move[0])),
+            max(0, min(self.grid_size - 1, self.state[1] + move[1]))
+        )
+
+        self.path_dir[self.i] = new_state
+        self.i += 1
+
         if new_state in self.holes:
-            return new_state, -1, True
+            reward = -1
+            done = True
         elif new_state == self.goal:
-            return new_state, 1, True
+            reward = 1
+            done = True
+            # Store the shortest path
+            if self.store_path:
+                self.path_store_dir = self.path_dir.copy()
+                self.store_path = False
+                self.shortest = len(self.path_dir)
+                self.longest = len(self.path_dir)
+            elif len(self.path_dir) < self.shortest:
+                self.shortest = len(self.path_dir)
+                self.path_store_dir = self.path_dir.copy()
+            elif len(self.path_dir) > self.longest:
+                self.longest = len(self.path_dir)
         else:
-            self.state = new_state
-            return new_state, 0, False
+            reward = 0
+            done = False
+
+        self.state = new_state
+        state_index = self.position_transition(*new_state)
+        return state_index, reward, done
+
 
     def render(self):
         self.window.blit(self.bg_img, (0, 0))
@@ -141,4 +179,15 @@ class FrozenLakeEnv:
                     self.window.blit(self.robot_img, pos)
         pygame.display.update()
 
+    def final(self): 
+        print('shortest path:', self.shortest)
+        print('longest path:', self.longest)
+        print('the shortest route is shown in red')
+
+        for j in range(len(self.path_store_dir)):
+            pos = self.path_store_dir[j]
+            x = pos[1] * self.cell_size + self.cell_size // 2
+            y = pos[0] * self.cell_size + self.cell_size // 2
+            pygame.draw.circle(self.window, (255, 0, 0), (x, y), 5)
+        pygame.display.update()
 
